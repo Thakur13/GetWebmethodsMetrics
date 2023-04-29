81a90f5987036493b931514253ba6ef351c1b24c
Thakur13 <asthakur185@gmail.com>
2023-04-29 10:02:04 +0530

    Initial commit

---
+++ src/main/java/dev/cloudhandson/webmethods/GetWebmethodsMetricsApplication.java
@@ -0,0 +1,13 @@
+package dev.cloudhandson.webmethods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetWebmethodsMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetWebmethodsMetricsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cloudhandson/webmethods/GetWebmethodsMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cloudhandson.webmethods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetWebmethodsMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
