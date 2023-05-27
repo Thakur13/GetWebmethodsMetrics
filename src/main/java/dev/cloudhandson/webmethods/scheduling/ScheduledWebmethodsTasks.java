b822bf587eb9e82bc14e8244c692d8284b710ead
Thakur13 <asthakur185@gmail.com>
2023-05-27 23:45:02 +0530

    Added method for retrieving UM Configuration

--- src/main/java/dev/cloudhandson/webmethods/scheduling/ScheduledWebmethodsTasks.java
+++ src/main/java/dev/cloudhandson/webmethods/scheduling/ScheduledWebmethodsTasks.java
@@ -5,6 +5,7 @@ import com.wm.app.b2b.client.ServiceException;
 import dev.cloudhandson.webmethods.metrics.generator.WebmethodsMetricsGenerator;
 import dev.cloudhandson.webmethods.model.AppProperties;
 import dev.cloudhandson.webmethods.model.IntegrationServerConfiguration;
+import dev.cloudhandson.webmethods.model.UniversalMessagingConfiguration;
 import dev.cloudhandson.webmethods.util.IntegrationServerConnectionProvider;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -13,13 +14,12 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Component
 public class ScheduledWebmethodsTasks {
 
-    private static final Logger isLog = LogManager.getLogger(WebmethodsMetricsGenerator.class);
+    private static final Logger isLog = LogManager.getLogger(ScheduledWebmethodsTasks.class);
 
     @Autowired
     private AppProperties appProperties;
@@ -55,15 +55,35 @@ public class ScheduledWebmethodsTasks {
                 } catch (Exception e) {
                     isLog.error("Error during processing : " + isConfig.getHost() + " : " + e.getMessage());
                 } finally {
-                  if (context != null) {
-                      isLog.info("Closing connection to Integration Server [" + isConfig.getAlias() + "]");
-                      context.disconnect();
-                      isLog.info("Connection closed");
-                  }
+                    if (context != null) {
+                        isLog.info("Closing connection to Integration Server [" + isConfig.getAlias() + "]");
+                        context.disconnect();
+                        isLog.info("Connection closed");
+                    }
                 }
             }
         } else {
             isLog.info("There is no Integration Server alias configured");
+        }
+    }
+
+    @Scheduled(fixedRateString = "${scheduling.fixed-rate.in.milliseconds}")
+    public void generateUniversalMessagingMetrics() {
+        isLog.debug("Executing generateUniversalMessagingMetrics ");
+        isLog.info("Thread name = " + Thread.currentThread().getName() + " time = " + LocalDateTime.now());
+
+        isLog.info("Retrieving the list of the Universal Messaging");
+
+        // List of Universal Messaging aliases
+        List<UniversalMessagingConfiguration> umList = appProperties.getUmList();
+
+        // Loop on all Universal Messaging Servers
+        if (umList != null) {
+            for (UniversalMessagingConfiguration umConfig : umList) {
+                isLog.info(umConfig.getAlias());
+            }
+        } else {
+            isLog.info("There is no Universal Messaging alias configured");
         }
     }
 }
